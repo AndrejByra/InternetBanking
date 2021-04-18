0f0f9ac2d1edf1fb92294b29c6f84aebb9967cd1
Andrej Byra <andrej.byraa@gmail.com>
2021-04-18 12:16:26 +0000

    Update src/main/java/com/core/internetbanking/service/BankAccountService.java

--- src/main/java/com/core/internetbanking/service/BankAccountService.java
+++ src/main/java/com/core/internetbanking/service/BankAccountService.java
@@ -9,27 +9,33 @@ public interface BankAccountService {
      * return bank account
      */
     BankAccount createAccount(Integer accountId, Integer balance);
+
     /**
      * method to get balance of account
      * return amount that is on account
      */
     Integer getBalanceOfAccount(Integer id);
+
     /**
      * method to send amount
      */
     void sendAmount(PaymentDto paymentDto) throws BackendException;
+
     /**
      * method to block bank account
      */
     void blockAccount(Integer accountId);
+
     /**
      * method to unblock bank account
      */
     void unblockAccount(Integer accountId);
+
     /**
      * method to set status of account to credit status
      */
     void setCreditCardStatus(Integer accountId);
+    
     /**
      * method to set status of account to debit status
      */
